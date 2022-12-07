c75de7913162493b6de2936756427d8be9e5a0ab
usuario2gag <tigresguille@hotmail.com>
2022-12-07 11:33:09 +0100

    Otras correcciones de código sobre el coste de los telegramas.

--- Depuración GAG 22-23/Form1.cs
+++ Depuración GAG 22-23/Form1.cs
@@ -31,13 +31,13 @@ namespace Depuración_GAG_22_23
             else
                 tipoTelegrama = 'o';
             //Obtengo el número de palabras que forma el telegrama
-            numPalabras = textoTelegrama.Length;
+            numPalabras = textoTelegrama.Split(' ').Length;
             //Si el telegrama es ordinario
             if (tipoTelegrama == 'o')
                 if (numPalabras <= 10)
-                    coste = 25;
+                    coste = 2.5;
                 else
-                    coste = 0.5 * numPalabras;
+                    coste = 2.5 + 0.5 * (numPalabras - 10);
             else
             //Si el telegrama es urgente
             if (tipoTelegrama == 'u')
